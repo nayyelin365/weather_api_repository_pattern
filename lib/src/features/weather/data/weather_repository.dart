cab52c705a57101e1335a5a186a58916d13ce1b6
nayye5154 <nayye5154@gmail.com>
2024-06-30 23:48:29 +0700

    change

--- lib/src/features/weather/data/weather_repository.dart
+++ lib/src/features/weather/data/weather_repository.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:open_weather_example_flutter/src/api/api.dart';
-import 'package:open_weather_example_flutter/src/api/api_keys.dart';
+import 'package:open_weather_example_flutter/src/api/api_keys.default.dart';
 import 'package:open_weather_example_flutter/src/features/weather/data/api_exception.dart';
 import 'package:open_weather_example_flutter/src/features/weather/domain/forecast/forecast.dart';
 import 'package:open_weather_example_flutter/src/features/weather/domain/weather/weather.dart';
